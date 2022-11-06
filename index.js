f2937e47ef0474b7f3353068d0ddcea3b28dd045
Robingeorge12 <robingsa.btch@gmail.com>
2022-11-06 20:26:08 +0530

    backend

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const  {connection} = require("./db")
 const {signupRouter} = require("./route/Sign.route")
 const { loginRouter } = require("./route/Login.route")
 const {noteRouter} = require("./route/Notes.route")
+var cors = require('cors')
+app.use(cors())
 app.use(express.json())
 
+
+app.get("/",(req,res)=>{
+
+    res.send("welcome to TODO")
+})
 
 app.use("/sign",signupRouter)
 app.use("/login",loginRouter)
